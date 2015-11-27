c7a76f744729bf5392e294f9d6b5dd66de64e0c9
brice180 <brice180@gmail.com>
2015-11-27 00:18:25 -0200

    hola

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+@extends('layout')
+
+@section('content')
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-8 col-md-offset-2">
+			<div class="panel panel-default">
+				<div class="panel-heading">Bem-vindo</div>
+				<div class="panel-body">
+					<p>Bem- Vindo ao Sistema..!!</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+@endsection
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,60 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Laravel</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="{{asset('css/app.css')}}">
+        <style>
+            html, body {
+                height: 100%;
+            }
+
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                display: table;
+                font-weight: 100;
+                font-family: 'Lato';
+            }
+
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+            .title {
+                font-size: 96px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="title">BEBIDAS ASA BRANCA</div>
+                </div>
+
+            <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">
+                                    Entrar ao Sistema
+                                </button>
+                            </div>
+            </div>
+
+        </div>
+    <!-- Scripts -->
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
+    @yield('scripts')
+    </body>
+</html>
